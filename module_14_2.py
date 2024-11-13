1c9cebba8257830e92924be2f11cc98ac1f6bb23
nksound <NKSound@mail.ru>
2024-11-13 14:46:50 +0300

    "Выбор элементов и функции в SQL запросах"

---
+++ module_14_2.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+connection = sqlite3.connect("not_telegram.db")
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL)''')
+
+cursor.execute("DELETE FROM Users WHERE id=?", (6,))
+connection.commit()
+
+cursor.execute("SELECT SUM(balance) FROM Users")
+all_balances = cursor.fetchone()[0]
+cursor.execute("SELECT COUNT(*) FROM Users")
+total_users = cursor.fetchone()[0]
+print(all_balances/total_users)
+
+connection.close()
